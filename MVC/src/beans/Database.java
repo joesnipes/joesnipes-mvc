e38d9654ecf970337f46fbb6b0618a0fa78f90f1
joesnipes <joesnipes@gmail.com>
2015-11-04 13:01:56 -0500

    Project File Upload
    added all project files to date for master

---
+++ MVC/src/beans/Database.java
@@ -0,0 +1,32 @@
+package beans;
+
+public class Database {
+
+	private String dbConn = "";
+	private String dbUser1 = "";
+	private String dbPass = "";
+	
+	
+	public String getDbConn() {
+		return dbConn;
+	}
+	public void setDbConn(String dbConn) {
+		this.dbConn = dbConn;
+	}
+	public String getDbUser1() {
+		return dbUser1;
+	}
+	public void setDbUser1(String dbUser1) {
+		this.dbUser1 = dbUser1;
+	}
+	public String getDbPass() {
+		return dbPass;
+	}
+	public void setDbPass(String dbPass) {
+		this.dbPass = dbPass;
+	}
+	
+	
+	
+	
+}
